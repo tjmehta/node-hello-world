09922d441cc14c2bd86dd1eb26b2c450e024b759
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-04-03 15:33:26 -0700

    CORS

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ if (!process.env.PORT) {
   throw new Error('process.env.PORT is required!');
 }
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Access-Control-Allow-Origin': '*'
+  });
   res.end('Hello World!');
 }).listen(process.env.PORT);
 console.log('Server running at http://127.0.0.1:'+process.env.PORT+'/');
