08bd275a70715c1e66bae5b4de2526b7bbdad1db
Anton Podviaznikov <podviaznikov@gmail.com>
2015-05-27 15:39:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 if (!process.env.PORT) {
-  throw new Error('process.env.PORT is required>>!');
+  throw new Error('process.env.PORT is required>>2!');
 }
 http.createServer(function (req, res) {
   res.writeHead(200, {
